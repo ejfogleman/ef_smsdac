5494fe215f007b84e361623a9b21ae04579b01ca
Eric Fogleman <82043426+ejfogleman@users.noreply.github.com>
2024-04-17 15:39:13 -0400

    Static code 0x70 test at cycle 3, 4

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ async def test_project(dut):
   dut._log.info("Start")
   
   # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 10, units="us")
+  clock = Clock(dut.clk, 1, units="us")
   cocotb.start_soon(clock.start())
 
   # Reset
@@ -23,10 +23,16 @@ async def test_project(dut):
   dut.rst_n.value = 1
 
   # Set the input values, wait one clock cycle, and check the output
+
+  # apply input of 112 (0x70) and check after 3, 4 cycles
   dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+  dut.ui_in.value = 112  # 0x70 just below midscale input
+  dut.uio_in.value = 0
+
+  await ClockCycles(dut.clk, 3)
+
+  assert dut.uo_out.value == 63   # 0x3F on cycle 3
 
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 50
+  assert dut.uo_out.value == 84   # 0x54 on cycle 4
