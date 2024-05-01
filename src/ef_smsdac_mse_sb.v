725228b22e377a8029a68d2943868fec4b71745f
Eric Fogleman <82043426+ejfogleman@users.noreply.github.com>
2024-05-01 12:11:38 -0400

    update license

--- src/ef_smsdac_mse_sb.v
+++ src/ef_smsdac_mse_sb.v
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 Eric Fogleman
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * Switching block for fully-segmented mismatch shaping encoder
  * from SEGMENTED MISMATCH-SHAPING D/A CONVERSION, 
