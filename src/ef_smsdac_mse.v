adf8260d7da10738bb9a23ed39e81e9a67d7f401
Eric Fogleman <82043426+ejfogleman@users.noreply.github.com>
2024-05-09 18:00:15 -0400

    Revert license

--- src/ef_smsdac_mse.v
+++ src/ef_smsdac_mse.v
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 Eric Fogleman
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Fully-segmented mismatch shaping encoder 
  * from SEGMENTED MISMATCH-SHAPING D/A CONVERSION, 
